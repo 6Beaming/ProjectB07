501d81957486bb35169e51f571a5917bb0c0cc68
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-07-17 21:01:41 -0400

    feat: Add model classes for gson to parse json file

---
+++ app/src/main/java/com/group15/b07project/Question.java
@@ -0,0 +1,9 @@
+package com.group15.b07project;
+
+import java.util.List;
+
+public class Question {
+    public String id, type, text;
+    public List<String> options;
+    public String followupTextPrompt;  // for single+text
+}
---
+++ app/src/main/java/com/group15/b07project/QuestionsBundle.java
@@ -0,0 +1,10 @@
+package com.group15.b07project;
+
+import java.util.List;
+import java.util.Map;
+
+public class QuestionsBundle {
+    public List<Question> warmUp;
+    public Map<String, List<Question>> branch;
+    public List<Question> followUp;
+}
