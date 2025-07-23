7eb486883d5841e43af43e72451c13b8a63ef244
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-07-23 19:07:15 -0400

    refactor: add followupTextPrompt to text types so users can have a prompt in the input field

--- app/src/main/java/com/group15/b07project/Question.java
+++ app/src/main/java/com/group15/b07project/Question.java
@@ -5,5 +5,5 @@ import java.util.List;
 public class Question {
     public String id, type, text;
     public List<String> options;
-    public String followupTextPrompt;  // for single+text
+    public String followupTextPrompt;  // pre-prompt shown in input field for users to text
 }
