43c8d6b27e0c1fc18f36ad3fda2b981f03e748ea
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-07-31 03:40:09 -0400

    feature: add basic implementation

--- app/src/main/java/com/group15/b07project/LaunchActivity.java
+++ app/src/main/java/com/group15/b07project/LaunchActivity.java
@@ -1,4 +1,26 @@
 package com.group15.b07project;
 
-public class LaunchActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LaunchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+
+        // finish so Back never returns here
+        finish();
+    }
 }
