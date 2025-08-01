194257dcc036376996fdcb21648d45d96adaca8a
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-08-01 02:14:19 -0400

    feature: Add PinLoginActivity.java & PinSetupActivity.java

---
+++ app/src/main/java/com/group15/b07project/PinLoginActivity.java
@@ -0,0 +1,6 @@
+package com.group15.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PinLoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/group15/b07project/PinSetupActivity.java
@@ -0,0 +1,6 @@
+package com.group15.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PinSetupActivity extends AppCompatActivity {
+}
