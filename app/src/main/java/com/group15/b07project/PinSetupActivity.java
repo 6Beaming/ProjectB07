550d1be84d31718e5d60358f4d58c0f6681ea16d
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-08-01 02:44:25 -0400

    docs: add documentation

--- app/src/main/java/com/group15/b07project/PinSetupActivity.java
+++ app/src/main/java/com/group15/b07project/PinSetupActivity.java
@@ -2,5 +2,9 @@ package com.group15.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// Ask the user to type their PIN twice to confirm
+// Securely store it as "pin_"+uid as key in SharedPreferences
+// This page will only show on the first sign up -> can be used to confirm if this is user's sign up
 public class PinSetupActivity extends AppCompatActivity {
+
 }
