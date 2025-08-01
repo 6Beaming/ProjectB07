e44747b1264e4a325e5ec64fb012330ceaa578ea
Eric Liu <ericb.liu@mail.utoronto.ca>
2025-08-01 19:24:59 -0400

    feature: add PIN login logic

--- app/src/main/java/com/group15/b07project/PinLoginActivity.java
+++ app/src/main/java/com/group15/b07project/PinLoginActivity.java
@@ -1,6 +1,38 @@
 package com.group15.b07project;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+// An activity that is launched by AuthChoiceActivity(prerequisite is uid is Non null)
 public class PinLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_login);
+
+        String uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+        EditText pinInput  = findViewById(R.id.pinInput);
+        Button buttonUnlock = findViewById(R.id.buttonUnlock);
+        PinManager pinManager = new PinManager(this);
+
+        buttonUnlock.setOnClickListener(v -> {
+            String typed  = pinInput.getText().toString();
+            if (pinManager.verifyPin(uid, typed)) {
+                // PIN verified -> main
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            } else {
+                pinInput.setError("Wrong PIN");
+            }
+        });
+    }
 }
