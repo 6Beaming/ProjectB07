92254fdc98fee842e1b938efa8affcbe798e3679
zhengqin2006 <3596239620@qq.com>
2025-08-02 15:14:01 -0400

    feat: add logout

--- app/src/main/java/com/group15/b07project/HomeFragment.java
+++ app/src/main/java/com/group15/b07project/HomeFragment.java
@@ -1,16 +1,25 @@
 package com.group15.b07project;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeFragment extends Fragment {
+    private FirebaseAuth firebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,8 +39,19 @@ public class HomeFragment extends Fragment {
 
         buttonSupport.setOnClickListener(v -> loadFragment(new SupportConnectionFragment()));
 
-        //buttonLogout.setOnClickListener(v -> loadFragment(new LogoutFragment()));
-
+        buttonLogout.setOnClickListener(v -> {
+            firebaseAuth = FirebaseAuth.getInstance();
+            //Log out
+            firebaseAuth.signOut();
+            // Reset shared preferences
+            SharedPreferences preference = requireActivity().getSharedPreferences("app_prefs", Context.MODE_PRIVATE);
+            preference.edit().putBoolean("logged_in", false).apply();
+            Toast.makeText(getContext(), "Logout successful， redirecting to login page", Toast.LENGTH_SHORT).show();
+            //Redirect to LaunchActivity
+            Intent intent = new Intent(requireActivity(), LaunchActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        });
         return view;
     }
 
